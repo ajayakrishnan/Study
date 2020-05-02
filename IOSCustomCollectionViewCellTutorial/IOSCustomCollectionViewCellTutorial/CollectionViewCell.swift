5533ea1fba7f1d2bb5609525de7e0b52057bd064
Ajaya Krishnan <ajayakrishnan@appstudio35.com>
2020-05-02 19:46:26 +0530

    New project

---
+++ IOSCustomCollectionViewCellTutorial/IOSCustomCollectionViewCellTutorial/CollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  CollectionViewCell.swift
+//  IOSCustomCollectionViewCellTutorial
+//
+//  Created by Ajay on 02/05/20.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var labelView: UILabel!
+    
+}
